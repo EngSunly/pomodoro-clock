66d467c0a8ab342329717dc93e995ea6e34c5144
SUNLY ENG <1220.eng.sunly@rupp.edu.kh>
2023-09-18 14:04:37 +0700

    yes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from 'react';
 import './App.css';
-import { clear } from '@testing-library/user-event/dist/clear';
+
 
 
 // clock lifecylce is 25 minutes work, 5 minutes break
@@ -16,41 +16,64 @@ function PomodoroClock() {
   const [seconds, setSeconds] = useState("00");
   const [isRunning, setIsRunning] = useState(false);
   const [intervalId, setIntervalId] = useState(null);
+  const [mode, setMode] = useState("work");// work or break;
+
+  // call once when component is mounted
+  let intervalRef = useRef(intervalId);
+  let minutesRef = useRef();
+  let secondsRef = useRef(parseInt());
+  const switchmodeRef = useRef();
+  function updateref() {
+    minutesRef.current = minutes;
+    secondsRef.current = parseInt(seconds);
+    switchmodeRef.current = switchmode;
+  }
 
 
   function handleStartPause() {
+    updateref();
     if (isRunning) {
       pauseTimer();
     } else {
       startTimer();
     }
   }
-
+  
   function startTimer() {
     setIsRunning(true);
   }
-
   function pauseTimer() {
     setIsRunning(false);
   }
 
-  // handling timer with useEffect and useRef
-  // from my understanding, useRef is used to store a value that will not change
-  // and will not cause a re-render
-  // in this case, we are storing the intervalId, minutes, and seconds
-  // we are using useEffect to update the timer every second
-  // we are using useRef to store the values of the timer
 
-  const intervalRef = useRef(intervalId);
-  const minutesRef = useRef(minutes);
-  const secondsRef = useRef(seconds);
+  function switchmode() {
+    if (mode === "work") {
+      setMinutes(breakLength);
+      setSeconds("00");
+      console.log("switching to break");
+      setIsRunning(false);
+      setMode("break");
+    } else {
+      setMinutes(workLength);
+      setSeconds("00");
+      updateref();
+      console.log("switching to work");
+      setIsRunning(false);
+      setMode("work");
+    }
+  }
 
   // this use effect is for the timer
   useEffect(() => {
     intervalRef.current = setInterval(() => {
-      if (secondsRef.current === "00") {
+      if (isRunning === false) {
+        return;
+      }
+      if (secondsRef.current === 0) {
         if (minutesRef.current === 0) {
           clearInterval(intervalRef.current);
+          switchmodeRef.current();
           return;
         }
         minutesRef.current--;
@@ -58,8 +81,9 @@ function PomodoroClock() {
       } else {
         secondsRef.current--;
       }
+      console.log("minutesRef.current = " + minutesRef.current + " secondsRef.current = " + secondsRef.current)
       setMinutes(minutesRef.current);
-      setSeconds(secondsRef.current);
+      setSeconds(secondsRef.current < 10 ? `0${secondsRef.current}` : secondsRef.current);
     }, 1000);
     return () => clearInterval(intervalRef.current);
   }, [isRunning]);
@@ -67,36 +91,48 @@ function PomodoroClock() {
 
 
 
-
-
-
-
-
-  function breakPlus() {
-    if (breakLength >= 60) { return }
-    setBreakLength(breakLength + 1);
-  }
-  function breakMinus() {
-    if (breakLength <= 1) { return }
-    setBreakLength(breakLength - 1);
-  }
-
-  function workPlus() {
-    if (workLength >= 60) { return }
-    setWorkLength(workLength + 1);
-  }
-
-  function workMinus() {
-    if (workLength <= 1) { return }
-    setWorkLength(workLength - 1);
-  }
-
   function resetButton() {
+    pauseTimer();
     setBreakLength(5);
     setWorkLength(25);
     setMinutes(25);
     setSeconds("00");
   }
+
+  function breakPlus() {
+    if (isRunning) { return }
+    if (breakLength >= 60) { return }
+    setBreakLength(breakLength + 1);
+    if (mode === "break") { setMinutes(breakLength - 1); }
+    updateref();
+  }
+
+  function breakMinus() {
+    if (isRunning) { return }
+    if (breakLength <= 1) { return }
+    setBreakLength(breakLength - 1);
+    if (mode === "break") { setMinutes(breakLength - 1); }
+    updateref();
+  }
+
+  function workPlus() {
+    if (isRunning) { return }
+    if (workLength >= 60) { return }
+    setWorkLength(workLength + 1);
+    if (mode === "work") { setMinutes(workLength - 1); }
+    updateref();
+  }
+
+  function workMinus() {
+    if (isRunning) { return }
+    if (workLength <= 1) { return }
+    setWorkLength(workLength - 1);
+    if (mode === "work") {
+      setMinutes(workLength - 1);
+    }
+    updateref();
+  }
+
 
   return (
     <div className="App">
